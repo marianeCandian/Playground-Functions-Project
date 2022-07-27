29c7e7c822bd9bd1aee21647a5d286dbecfa4314
Mariane Candian <mariane.candian@gmail.com>
2022-07-27 11:40:29 -0300

    Desafio 11, muito difícil

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,25 @@
+/* eslint-disable complexity */
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } 
+  let contando = 0;
+  for (let i of array) {
+    for (let index = 0; index < array.length; index += 1) {
+      if (i === array[index]) {
+        contando += 1;
+      }
+      if (contando >= 3 || i < 0 || i > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    contando = 0;
+  }
+  return ('(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10]);
 }
+
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
